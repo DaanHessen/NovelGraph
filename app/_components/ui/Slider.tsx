792b11e4943fcc3812301c0249afad59db9fe168
Daan Hessen <daanh2002@gmail.com>
2026-01-11 13:03:42 +0100

    -_-

--- app/_components/ui/Slider.tsx
+++ app/_components/ui/Slider.tsx
@@ -12,6 +12,9 @@ interface SliderProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 
 
 const Slider = React.forwardRef<HTMLInputElement, SliderProps>(
     ({ className, value, onChange, min = 0, max = 100, step = 1, ...props }, ref) => {
+        // Calculate percentage for the gradient background to simulate a fill track
+        const percentage = ((value - min) / (max - min)) * 100;
+
         return (
             <div className={`relative flex items-center w-full h-8 touch-none select-none ${className}`}>
                  <input 
@@ -21,7 +24,22 @@ const Slider = React.forwardRef<HTMLInputElement, SliderProps>(
                     step={step}
                     value={value}
                     onChange={(e) => onChange(parseFloat(e.target.value))}
-                    className="flex-1 w-full h-2 bg-secondary rounded-full appearance-none cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-primary [&::-webkit-slider-thumb]:border-2 [&::-webkit-slider-thumb]:border-primary hover:[&::-webkit-slider-thumb]:bg-primary/90 transition-all focus-visible:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
+                    style={{
+                        background: `linear-gradient(to right, var(--primary) 0%, var(--primary) ${percentage}%, var(--secondary) ${percentage}%, var(--secondary) 100%)`
+                    }}
+                    className="flex-1 w-full h-1.5 rounded-full appearance-none cursor-pointer 
+                        bg-secondary 
+                        [&::-webkit-slider-thumb]:appearance-none 
+                        [&::-webkit-slider-thumb]:w-4 
+                        [&::-webkit-slider-thumb]:h-4 
+                        [&::-webkit-slider-thumb]:rounded-full 
+                        [&::-webkit-slider-thumb]:bg-foreground 
+                        [&::-webkit-slider-thumb]:shadow-lg 
+                        [&::-webkit-slider-thumb]:transition-all 
+                        [&::-webkit-slider-thumb]:border-none 
+                        hover:[&::-webkit-slider-thumb]:scale-110 
+                        focus-visible:outline-none 
+                        focus:ring-2 focus:ring-accent focus:ring-offset-background  focus:ring-offset-2"
                     ref={ref}
                     {...props}
                 />
